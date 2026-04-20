d17b3132e19ba53e7a96169ff65ce3cbf8f4f539
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-19 23:24:04 -0500

    feat: numero mesa en mesa

--- src/restauranteelbuensabor/Mesa.java
+++ src/restauranteelbuensabor/Mesa.java
@@ -1,4 +1,24 @@
 package restauranteelbuensabor;
 
+/**
+ * Representa el estado de la mesa actual del restaurante.
+ */
+
 public class Mesa {
+
+    private static int  numeroMesaActual = 0;
+    private static boolean activa        = false;
+
+    public static int  getNumero()   { return numeroMesaActual; }
+    public static boolean estaActiva() { return activa; }
+
+    public static void activar(int numero) {
+        numeroMesaActual = numero > 0 ? numero : 1;
+        activa = true;
+    }
+
+    public static void reiniciar() {
+        numeroMesaActual = 0;
+        activa = false;
+    }
 }
