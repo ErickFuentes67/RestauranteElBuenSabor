5765f8520b9288457fb473686ee3744157c658a5
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-19 22:36:32 -0500

    Refactor: metodos calcular subtotal removidos de producto

--- src/restauranteelbuensabor/Producto.java
+++ src/restauranteelbuensabor/Producto.java
@@ -7,27 +7,14 @@ public class Producto {
 
     private final String nombre;
     private final double precio;
-    private int cantidad;
 
     public Producto(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio;
-        this.cantidad = 0;
+
     }
 
     public String getNombre()   { return nombre; }
     public double getPrecio()   { return precio; }
-    public int    getCantidad() { return cantidad; }
 
-    public void agregarCantidad(int cantidad) {
-        this.cantidad += cantidad;
-    }
-
-    public void reiniciar() {
-        this.cantidad = 0;
-    }
-
-    public double calcularSubtotal() {
-        return precio * cantidad;
-    }
 }
