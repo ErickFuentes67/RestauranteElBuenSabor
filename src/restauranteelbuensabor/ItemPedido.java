06be1346a5a6888b55c9275141f4e3613353cd21
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-19 22:25:16 -0500

    feat: itemPedido variable producto y cantidad

--- src/restauranteelbuensabor/ItemPedido.java
+++ src/restauranteelbuensabor/ItemPedido.java
@@ -2,6 +2,24 @@ package restauranteelbuensabor;
 
 public class ItemPedido {
 
+    private final Producto producto;
+    private int cantidad;
+
+    public ItemPedido(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto()  { return producto; }
+    public int      getCantidad()  { return cantidad; }
+
+    public void agregarCantidad(int cantidad) {
+        this.cantidad += cantidad;
+    }
+
+    public double calcularSubtotal() {
+        return producto.getPrecio() * cantidad;
+    }
 
 
 }
