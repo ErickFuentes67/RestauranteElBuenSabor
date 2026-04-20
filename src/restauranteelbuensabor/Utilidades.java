3261e7da4e430264d35d23ce867def2ffed2dfcb
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-19 21:07:07 -0500

    Refactor: Eliminacion metodo calcular() inncecesario en Utilidades

--- src/restauranteelbuensabor/Utilidades.java
+++ src/restauranteelbuensabor/Utilidades.java
@@ -14,24 +14,6 @@ package restauranteelbuensabor;
 
 public class Utilidades {
 
-    public static double calcular(double pr,double cn,double dc,double iv,double pp,int ni,boolean ap){
-
-        double res=0;double tmp=0;double aux2=0;
-
-        // calcula el resultado
-        res=pr*cn;
-
-        if(dc>0){res=res-(res*dc);}
-        tmp=res*iv;res=res+tmp;
-        if(ap){res=res+(res*pp);}
-
-        // imprime restaurante
-        System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
-        aux2=res;
-        return aux2;
-
-    }
-
     public static boolean validar(){
 
         int cont=0;int indice=0;
@@ -42,7 +24,7 @@ public class Utilidades {
 
         // reinicia si no hay nada - efecto secundario no documentado
         if(cont==0){
-            Datos.total=0;Datos.tmp="";
+            Datos.total=0;
         }
 
         return cont>0;
@@ -55,6 +37,6 @@ public class Utilidades {
             Datos.cantidades[indice]=0;indice++;
         }
 
-        Datos.total=0;Datos.estadoMesa=0;Datos.numeroMesaActual=0;Datos.tmp="";
+        Datos.total=0;Datos.estadoMesa=0;Datos.numeroMesaActual=0;
     }
 }
