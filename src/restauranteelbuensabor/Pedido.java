647e01b9452f427e5d3cad12f02a859d9b0b322e
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-19 22:28:48 -0500

    feat: metodo agregar item

--- src/restauranteelbuensabor/Pedido.java
+++ src/restauranteelbuensabor/Pedido.java
@@ -1,4 +1,22 @@
 package restauranteelbuensabor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+
+    private final List<ItemPedido> items = new ArrayList<>();
+
+    public void agregarItem(Producto producto, int cantidad) {
+        for (ItemPedido item : items) {
+            if (item.getProducto() == producto) {
+                item.agregarCantidad(cantidad);  // si ya existe, suma la cantidad
+                return;
+            }
+        }
+        items.add(new ItemPedido(producto, cantidad)); // si no existe, lo agrega
+    }
+
+
+
 }
